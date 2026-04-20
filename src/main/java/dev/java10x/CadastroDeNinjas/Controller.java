33ab6884f003bfa6fd4b141677aec3e2011d495b
renangth <renan.alima99@gmail.com>
2026-04-20 18:06:27 -0300

    feat: Adiciona annotations e o metodo boasVindas a classe Controller

--- src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -1,4 +1,14 @@
 package dev.java10x.CadastroDeNinjas;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController // anotation para especificar que é um Controller
+@RequestMapping // anotation para mapear as rotas e colocar todas no msm lugar
 public class Controller {
+
+    @GetMapping("/boasvindas") // pega as informaçoes de boas vindas e cria uma rota /boasVindas (boa prática: usar /)
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem na rota";
+    }
 }

20b0407bedff2b745fa0f18c7c1366e04a63b4a5
Renan Alves <renan.alima99@gmail.com>
2026-08-14 13:51:28 -0300

    feat: Injeta a dependencia de MissoesRepository em MissoesService para utilizar o método de find all

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,4 +1,20 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository){
+        this.missoesRepository = missoesRepository;
+    }
+
+    // Listar as missoes
+    public List<MissoesModel> listarMissoes(){
+        return missoesRepository.findAll();
+    }
 }
